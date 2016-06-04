5fda3ae02fc86797a13e88c2961005e436018a1d
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2016-06-04 14:23:31 +0100

    Update db.js

--- database/db.js
+++ database/db.js
@@ -121,7 +121,7 @@ var handler = {
 
 	    var ptab = new Promise((resolve, reject) => {
 	    	
-	    var getTable = (x, t) =>t.any("SELECT * FROM $1~", [x])
+	    var getTable = (x, t) =>t.any("SELECT * FROM $1~", x)
                 .then(data => ({name: x, data: data}));
 
             db.task(t => t.batch(req.body.tables.map(x => getTable(x, t))))
