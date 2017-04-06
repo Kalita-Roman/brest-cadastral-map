9d967cbef536a42560659669bd581f813da16924
Roman Kalita <z11z22z33@mail.ru>
2017-04-06 19:28:12 +0300

    brest-map.heroku

--- database/connectionPostgreSQL.js
+++ database/connectionPostgreSQL.js
@@ -1,23 +1,13 @@
 var pgp = require("pg-promise")();
-/*
-var connection = {
-    	host: 'localhost',
-    	port: 5432,
-		database: 'brestdb',
-    	user: 'postgres',
-    	password: 'root'
-	};
-*/
 
-var connection = {
-        host: 'ec2-54-243-204-195.compute-1.amazonaws.com',
-        port: 5432,
-        database: 'degu98g3me9l74',
-        user: 'iqujckjxjswxii',
-        password: '_DSzPBjQ3VAVk_gZTGP7lgogir',
-        ssl: true
-    };
-
+const connection = {
+	host: 'ec2-54-225-121-93.compute-1.amazonaws.com',
+	port: 5432,
+	database: 'd7ju1tiq6pteid',
+	user: 'vzdjklcygtbeta',
+	password: '5O0R9b4ydXWPbnQs6BGR6ZswvY',
+	ssl: true
+};
 
 
 module.exports = pgp(connection);
